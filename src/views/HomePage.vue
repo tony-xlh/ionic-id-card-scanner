2724524209a46f6108b91d7f8fbc7f8a168a2836
xulihang <xulihanghai@163.com>
2023-09-15 14:15:58 +0800

    add a capture button in home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,55 +2,54 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>ID Scanner</ion-title>
       </ion-toolbar>
     </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
     </ion-content>
+    <ion-footer>
+      <div class="toolbar">
+        <button class="button round" @click="scan">Scan</button>
+      </div>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, useIonRouter } from '@ionic/vue';
+
+const router = useIonRouter();
+
+const scan = () => {
+  router.push("/scanner")
+}
 </script>
 
 <style scoped>
-#container {
+.button {
+  background-color: #3880ff;
+  border: none;
+  color: white;
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
   text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  width: 50px;
+  height: 50px;
+  transform: translateY(-10px);
+}
+
+.button:hover {
+  background-color: #79a8f9;
+}
+
+.round{
+  border-radius: 50%;
+}
+
+.toolbar {
+  display: flex;
+  justify-content: center;
+  height: 40px;
 }
 </style>
