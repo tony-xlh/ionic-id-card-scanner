16d28e17f331ed3b22cfdbe375d6c6bf35bb224a
xulihang <xulihanghai@163.com>
2023-09-19 15:31:35 +0800

    add box shadow for toolbar

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -84,5 +84,6 @@ const cardClicked = (index:number) => {
   display: flex;
   justify-content: center;
   height: 40px;
+  box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
 }
 </style>
